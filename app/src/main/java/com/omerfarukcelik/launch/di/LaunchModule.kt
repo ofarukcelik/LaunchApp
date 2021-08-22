6627334fccc24f8340433b018f3b920fc8ece258
Ömer <ocelik@mgssoftware.net>
2021-08-23 02:29:21 +0300

    fixed naming.

--- app/src/main/java/com/omerfarukcelik/launch/di/LaunchModule.kt
+++ app/src/main/java/com/omerfarukcelik/launch/di/LaunchModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+object LaunchModule {
   val BASE_URL = "https://api.spacexdata.com"
 
   @Singleton
